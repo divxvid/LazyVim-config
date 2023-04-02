cf7b87183a2d6bf8edb037e2e4f2d224ed35e6f8
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-04-02 15:07:00 +0530

    Disable italics and add jk binding

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+    },
+  },
+}

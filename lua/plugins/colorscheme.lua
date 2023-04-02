08d8452a73cd930bf3155c82cc43e1b08d4ab4e3
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-04-02 15:58:36 +0530

    Add transparent background for tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ return {
     "folke/tokyonight.nvim",
     lazy = true,
     opts = {
+      transparent = true, -- Enable this to disable setting the background color
       styles = {
         comments = { italic = false },
         keywords = { italic = false },

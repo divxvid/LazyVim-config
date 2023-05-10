32c8cc8dfccf3454bf09d16582a42aa4b0df4938
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-05-10 12:43:33 +0530

    Add Catpuccin colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,38 +10,84 @@ return {
   --     },
   --   },
   -- },
+  -- {
+  --   "https://github.com/rebelot/kanagawa.nvim",
+  --   opts = {
+  --     compile = false, -- enable compiling the colorscheme
+  --     undercurl = true, -- enable undercurls
+  --     commentStyle = { italic = false },
+  --     functionStyle = {},
+  --     keywordStyle = { italic = false },
+  --     statementStyle = { bold = true },
+  --     typeStyle = {},
+  --     transparent = true, -- do not set background color
+  --     dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --     terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  --     colors = { -- add/modify theme and palette colors
+  --       palette = {},
+  --       theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --     },
+  --     overrides = function(colors) -- add/modify highlights
+  --       return {}
+  --     end,
+  --     theme = "wave", -- Load "wave" theme when 'background' option is not set
+  --     background = { -- map the value of 'background' option to a theme
+  --       -- dark = "wave", -- try "dragon" !
+  --       dark = "wave",
+  --       light = "lotus",
+  --     },
+  --   },
+  -- },
   {
-    "https://github.com/rebelot/kanagawa.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     opts = {
-      compile = false, -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = false },
-      functionStyle = {},
-      keywordStyle = { italic = false },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = true, -- do not set background color
-      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
       },
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      theme = "wave", -- Load "wave" theme when 'background' option is not set
-      background = { -- map the value of 'background' option to a theme
-        -- dark = "wave", -- try "dragon" !
-        dark = "wave",
-        light = "lotus",
+      transparent_background = true,
+      show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+      term_colors = false,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false,
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa", --set the current theme to kanagawa
+      colorscheme = "catppuccin", --set the current theme to catppuccin
     },
   },
 }

4ae250e3faa956c3bc0f8d9ffc2209e5ff634f98
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-05-20 13:16:38 +0530

    Add Rose-pine colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,43 +1,43 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = true,
-  --   opts = {
-  --     transparent = true, -- Enable this to disable setting the background color
-  --     styles = {
-  --       comments = { italic = false },
-  --       keywords = { italic = false },
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "https://github.com/rebelot/kanagawa.nvim",
-  --   opts = {
-  --     compile = false, -- enable compiling the colorscheme
-  --     undercurl = true, -- enable undercurls
-  --     commentStyle = { italic = false },
-  --     functionStyle = {},
-  --     keywordStyle = { italic = false },
-  --     statementStyle = { bold = true },
-  --     typeStyle = {},
-  --     transparent = true, -- do not set background color
-  --     dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-  --     terminalColors = true, -- define vim.g.terminal_color_{0,17}
-  --     colors = { -- add/modify theme and palette colors
-  --       palette = {},
-  --       theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-  --     },
-  --     overrides = function(colors) -- add/modify highlights
-  --       return {}
-  --     end,
-  --     theme = "wave", -- Load "wave" theme when 'background' option is not set
-  --     background = { -- map the value of 'background' option to a theme
-  --       -- dark = "wave", -- try "dragon" !
-  --       dark = "wave",
-  --       light = "lotus",
-  --     },
-  --   },
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true, -- Enable this to disable setting the background color
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+    },
+  },
+  {
+    "https://github.com/rebelot/kanagawa.nvim",
+    opts = {
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = false },
+      functionStyle = {},
+      keywordStyle = { italic = false },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = "wave", -- Load "wave" theme when 'background' option is not set
+      background = { -- map the value of 'background' option to a theme
+        -- dark = "wave", -- try "dragon" !
+        dark = "wave",
+        light = "lotus",
+      },
+    },
+  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -47,7 +47,7 @@ return {
         light = "latte",
         dark = "mocha",
       },
-      transparent_background = true,
+      transparent_background = false,
       show_end_of_buffer = false, -- show the '~' characters after the end of buffers
       term_colors = false,
       dim_inactive = {
@@ -85,9 +85,63 @@ return {
     },
   },
   {
+    "rose-pine/neovim",
+    lazy = true,
+    name = "rose-pine",
+    opts = {
+      --- @usage 'auto'|'main'|'moon'|'dawn'
+      variant = "main",
+      --- @usage 'main'|'moon'|'dawn'
+      dark_variant = "main",
+      bold_vert_split = false,
+      dim_nc_background = false,
+      disable_background = false,
+      disable_float_background = false,
+      disable_italics = true,
+
+      --- @usage string hex value or named color from rosepinetheme.com/palette
+      groups = {
+        background = "base",
+        background_nc = "_experimental_nc",
+        panel = "surface",
+        panel_nc = "base",
+        border = "highlight_med",
+        comment = "muted",
+        link = "iris",
+        punctuation = "subtle",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        warn = "gold",
+
+        headings = {
+          h1 = "iris",
+          h2 = "foam",
+          h3 = "rose",
+          h4 = "gold",
+          h5 = "pine",
+          h6 = "foam",
+        },
+        -- or set all headings at once
+        -- headings = 'subtle'
+      },
+
+      -- Change specific vim highlight groups
+      -- https://github.com/rose-pine/neovim/wiki/Recipes
+      highlight_groups = {
+        ColorColumn = { bg = "rose" },
+
+        -- Blend colours against the "base" background
+        CursorLine = { bg = "foam", blend = 10 },
+        StatusLine = { fg = "love", bg = "love", blend = 10 },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin", --set the current theme to catppuccin
+      colorscheme = "rose-pine", --set the current theme to kanagawa
     },
   },
 }
